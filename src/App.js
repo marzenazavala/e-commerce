08cd4f93f01361436cfa8719c221e0679520e0f1
Marzena Bortnowska <zionscoders@gmail.com>
2020-05-03 17:20:06 +0200

    feat: signInform

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import {Switch, Route} from "react-router-dom";
 import HomePage from './pages/homePage/HomePage'
-import ShopPage from './pages/shop/ShopPage';
-import Header from './components/header/Header'
+import ShopPage from './pages/shopPage/ShopPage';
+import Header from './components/header/Header';
+import SignInSignUp from './pages/signIn-signUp/SigninSignUp'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/signin' component={SignInSignUp}/>
         <Route exact path='/tables' component={HomePage}/>
         <Route exact path='/sofas' component={HomePage}/>
         <Route exact path='/chairs' component={HomePage}/>
