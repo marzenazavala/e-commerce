165f00f91d891e8b3279bc218c62e5d1304f3b86
Marzena Bortnowska <zionscoders@gmail.com>
2020-05-25 11:57:54 +0200

    feat: signin with google and current user data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homePage/HomePage'
 import ShopPage from './pages/shopPage/ShopPage';
 import Header from './components/header/Header';
 import SignInSignUp from './pages/signIn-signUp/SigninSignUp';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends Component {
@@ -21,8 +21,22 @@ unsubscribeFromAuth = null
 
 
 componentDidMount() {
-  this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-    this.setState({currentUser: user})
+  this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    if(userAuth) {
+
+      const userRef = await createUserProfileDocument(userAuth)
+
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        })
+      });
+       
+    }
+    this.setState({currentUser: userAuth})
   })
 };
 
