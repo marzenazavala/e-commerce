da6a38781fbc7ae5672e634e030e2dc8c815d839
Marzena Bortnowska-Zavala <marzenazavala@gmail.com>
2020-07-18 15:09:36 +0200

    feat: counter added to cartIcon

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -4,15 +4,20 @@ import './cartIcon.scss';
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../redux/cart/cartActions'
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) =>  
+    accumulatedQuantity + cartItem.quantity, 0)
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
