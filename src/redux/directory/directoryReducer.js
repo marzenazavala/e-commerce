011a54344ed48e63de592ee9d6affb16fe7c7809
Marzena Bortnowska-Zavala <marzenazavala@gmail.com>
2020-07-24 15:30:01 +0200

    photos updated

--- src/redux/directory/directoryReducer.js
+++ src/redux/directory/directoryReducer.js
@@ -1,33 +1,33 @@
 const INITIAL_STATE = {
     sections:[
         {
-            title: "tables",
-            imageUrl: "https://cdn.home-designing.com/wp-content/uploads/2019/06/Low-Square-Modern-Minimalist-Coffee-Table-Metal-Silver-No-Assembly-Required-1024x683.jpg",
+            title: "hats",
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
             id: 1,
             linkUrl: 'shop/hats'
         },
         {
-            title: "sofas",
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/81P0-pvTSdL._AC_SX522_.jpg",
+            title: "sneakers",
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
             id: 2 ,
             linkUrl: 'shop/sneakers'
         },
         {
-            title: "chairs",
-            imageUrl: "https://cdn.shopify.com/s/files/1/2505/7782/products/sika-design-romantica-rattan-wicker-arm-chair-antique-lifestyle-photo_1571324808_1200x.jpg?v=1579096423",
+            title: "jackets",
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
             id: 3,
             linkUrl: 'shop/jackets'
         },
         {
-            title: "home",
-            imageUrl: "https://blog.modsy.com/wp-content/uploads/2019/11/658170_D1_LivingRoom_AllWhiteLivingRoom_Cam1.jpg",
+            title: "womens",
+            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             size: 'large',
             id: 4,
             linkUrl: 'shop/womens'
         },
         {
-            title: "garden",
-            imageUrl: "https://abeautifulspace.co.uk/wp-content/uploads/2015/07/sail-garden.jpg",
+            title: "mens",
+            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
             size: 'large',
             id: 5,
             linkUrl: 'shop/mens'
